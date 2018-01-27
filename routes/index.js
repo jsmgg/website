06e6e7156a0fda7f02c4fa7bab026ee19e253eb5
zhiming.hu <zhiming.hu@rjfittime.com>
2018-01-27 19:38:07 +0800

    a

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var tools = require('./util/tools.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+  	title: 'Express',
+  	isWap:tools.isWap(req.useragent)
+   });
 });
 
 module.exports = router;
